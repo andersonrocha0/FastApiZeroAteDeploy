a095a31e9d7481df54509c6375a8565ed27226df
Anderson Rocha <anderson.rocha@addesistemas.com.br>
2022-05-27 15:31:14 +0100

    Adiciona as primeiras REST APIs para listar e criar uma conta a pagar/receber

---
+++ contas_a_pagar_e_receber/routers/contas_a_pagar_e_receber_router.py
@@ -0,0 +1,48 @@
+from decimal import Decimal
+from typing import List
+
+from fastapi import APIRouter
+from pydantic import BaseModel
+
+router = APIRouter(prefix="/contas-a-pagar-e-receber")
+
+
+class ContaPagarReceberResponse(BaseModel):
+    id: int
+    descricao: str
+    valor: Decimal
+    tipo: str  # PAGAR, RECEBER
+
+
+class ContaPagarReceberRequest(BaseModel):
+    descricao: str
+    valor: Decimal
+    tipo: str  # PAGAR, RECEBER
+
+
+@router.get("/", response_model=List[ContaPagarReceberResponse])
+def listar_contas():
+    return [
+        ContaPagarReceberResponse(
+            id=1,
+            descricao="Aluguel",
+            valor=1000.50,
+            tipo="PAGAR"
+        ),
+        ContaPagarReceberResponse(
+            id=2,
+            descricao="Salário",
+            valor=5000,
+            tipo="RECEBER"
+        ),
+    ]
+
+
+@router.post("/", response_model=ContaPagarReceberResponse, status_code=201)
+def criar_conta(conta: ContaPagarReceberRequest):
+    return ContaPagarReceberResponse(
+        id=3,
+        descricao=conta.descricao,
+        valor=conta.valor,
+        tipo=conta.tipo
+    )
---
+++ main.py
@@ -0,0 +1,17 @@
+import uvicorn
+from fastapi import FastAPI
+
+from contas_a_pagar_e_receber.routers import contas_a_pagar_e_receber_router
+
+app = FastAPI()
+
+
+@app.get("/")
+def oi_eu_sou_programador() -> str:
+    return "Oi, eu sou um programador!"
+
+
+app.include_router(contas_a_pagar_e_receber_router.router)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8001)
