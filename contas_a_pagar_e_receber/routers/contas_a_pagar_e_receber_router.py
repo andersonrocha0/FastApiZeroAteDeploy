f5005daf886051702b141dcfbbbc0287bd84e4ae
Anderson Rocha <anderson.rocha@addesistemas.com.br>
2022-06-12 21:08:37 +0100

    Ajusta os testes apontando para um banco mock.

--- contas_a_pagar_e_receber/routers/contas_a_pagar_e_receber_router.py
+++ contas_a_pagar_e_receber/routers/contas_a_pagar_e_receber_router.py
@@ -1,8 +1,9 @@
 from decimal import Decimal
+from enum import Enum
 from typing import List
 
 from fastapi import APIRouter, Depends
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
 
 from contas_a_pagar_e_receber.models.conta_a_pagar_receber_model import ContaPagarReceber
@@ -21,10 +22,15 @@ class ContaPagarReceberResponse(BaseModel):
         orm_mode = True
 
 
+class ContaPagarReceberTipoEnum(str, Enum):
+    PAGAR = 'PAGAR'
+    RECEBER = 'RECEBER'
+
+
 class ContaPagarReceberRequest(BaseModel):
-    descricao: str
-    valor: Decimal
-    tipo: str  # PAGAR, RECEBER
+    descricao: str = Field(min_length=3, max_length=30)
+    valor: Decimal = Field(gt=0)
+    tipo: ContaPagarReceberTipoEnum  # PAGAR, RECEBER
 
 
 @router.get("", response_model=List[ContaPagarReceberResponse])
---
+++ test/contas_a_pagar_e_receber/routers/test_integrado_contas_a_pagar_e_receber_router.py
@@ -0,0 +1,108 @@
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from main import app
+from shared.database import Base
+from shared.dependencies import get_db
+
+client = TestClient(app)
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def override_get_db():
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+app.dependency_overrides[get_db] = override_get_db
+
+
+def test_deve_listar_contas_a_pagar_e_receber():
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+
+    client.post("/contas-a-pagar-e-receber", json={'descricao': 'Aluguel', 'valor': 1000.5, 'tipo': 'PAGAR'})
+    client.post("/contas-a-pagar-e-receber", json={'descricao': 'Salário', 'valor': 5000, 'tipo': 'RECEBER'})
+
+    response = client.get('/contas-a-pagar-e-receber')
+    assert response.status_code == 200
+    assert response.json() == [
+        {'id': 1, 'descricao': 'Aluguel', 'valor': 1000.5, 'tipo': 'PAGAR'},
+        {'id': 2, 'descricao': 'Salário', 'valor': 5000, 'tipo': 'RECEBER'}
+    ]
+
+
+def test_deve_criar_conta_a_pagar_e_receber():
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+
+    nova_conta = {
+        "descricao": "Curso de Python",
+        "valor": 333,
+        "tipo": "PAGAR"
+    }
+    nova_conta_copy = nova_conta.copy()
+    nova_conta_copy["id"] = 1
+
+    response = client.post("/contas-a-pagar-e-receber", json=nova_conta)
+    assert response.status_code == 201
+    assert response.json() == nova_conta_copy
+
+
+def test_deve_retornar_erro_quando_exceder_a_descricao():
+    response = client.post('/contas-a-pagar-e-receber', json={
+        "descricao": "0123456789012345678901234567890",
+        "valor": 333,
+        "tipo": "PAGAR"
+    })
+    assert response.status_code == 422
+    assert response.json()['detail'][0]['loc'] == ["body", "descricao"]
+
+
+def test_deve_retornar_erro_quando_a_descricao_for_menor_do_que_o_necessario():
+    response = client.post('/contas-a-pagar-e-receber', json={
+        "descricao": "01",
+        "valor": 333,
+        "tipo": "PAGAR"
+    })
+    assert response.status_code == 422
+    assert response.json()['detail'][0]['loc'] == ["body", "descricao"]
+
+
+def test_deve_retornar_erro_quando_o_valor_for_zero_ou_menor():
+    response = client.post('/contas-a-pagar-e-receber', json={
+        "descricao": "Test",
+        "valor": 0,
+        "tipo": "PAGAR"
+    })
+    assert response.status_code == 422
+    assert response.json()['detail'][0]['loc'] == ["body", "valor"]
+
+    response = client.post('/contas-a-pagar-e-receber', json={
+        "descricao": "Test",
+        "valor": -1,
+        "tipo": "PAGAR"
+    })
+    assert response.status_code == 422
+    assert response.json()['detail'][0]['loc'] == ["body", "valor"]
+
+
+def test_deve_retornar_erro_quando_o_tipo_for_invalido():
+    response = client.post('/contas-a-pagar-e-receber', json={
+        "descricao": "Test",
+        "valor": 100,
+        "tipo": "INVALIDO"
+    })
+    assert response.status_code == 422
+    assert response.json()['detail'][0]['loc'] == ["body", "tipo"]
