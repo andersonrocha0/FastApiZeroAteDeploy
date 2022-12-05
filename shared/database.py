c295b3dfed1b4a1bc95c31bd597cf6aff7ed3c76
Anderson Rocha <anderson.rocha@ausy.pt>
2022-12-05 14:51:32 +0000

    Prepara a aplicação para deploy com docker.

--- shared/database.py
+++ shared/database.py
@@ -1,9 +1,11 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1234@localhost/db_fast_api_zero_ate_deploy"
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
